de50c4a5fe43b5514ecc9d6d3acfa60100130f68
Asha <94jsasha@gmail.com>
2020-11-13 11:34:25 +0100

    Add exercise 03 extra-1

--- src/__tests__/exercise/03.js
+++ src/__tests__/exercise/03.js
@@ -5,6 +5,7 @@ import * as React from 'react'
 // 🐨 add `screen` to the import here:
 import { render, fireEvent, screen } from '@testing-library/react'
 import Counter from '../../components/counter'
+import userEvent from '@testing-library/user-event'
 
 test('counter increments and decrements when the buttons are clicked', () => {
   const { container } = render(<Counter />)
@@ -16,8 +17,8 @@ test('counter increments and decrements when the buttons are clicked', () => {
   //container.firstChild.querySelector('div')
 
   expect(message).toHaveTextContent('Current count: 0')
-  fireEvent.click(increment)
+  userEvent.click(increment)
   expect(message).toHaveTextContent('Current count: 1')
-  fireEvent.click(decrement)
+  userEvent.click(decrement)
   expect(message).toHaveTextContent('Current count: 0')
 })
