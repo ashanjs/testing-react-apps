43dba8160a37a2d11e6c7a47b6c529466b3fcb74
Asha <94jsasha@gmail.com>
2020-11-14 13:31:26 +0100

    Add exercise 06 extra-2

--- src/__tests__/exercise/06.js
+++ src/__tests__/exercise/06.js
@@ -1,50 +1,80 @@
 // mocking Browser APIs and modules
+// 💯 test the unhappy path
 // http://localhost:3000/location
 
-import * as React from 'react'
+import React from 'react'
 import { render, screen, act } from '@testing-library/react'
-import { useCurrentPosition } from 'react-use-geolocation'
 import Location from '../../examples/location'
 
-jest.mock('react-use-geolocation')
+beforeAll(() => {
+  window.navigator.geolocation = {
+    getCurrentPosition: jest.fn(),
+  }
+})
+
+function deferred() {
+  let resolve, reject
+  const promise = new Promise((res, rej) => {
+    resolve = res
+    reject = rej
+  })
+  return { promise, resolve, reject }
+}
 
 test('displays the users current location', async () => {
-
   const fakePosition = {
     coords: {
       latitude: 35,
-      longitude: 139
-    }
+      longitude: 139,
+    },
   }
-
-  let setReturnValue
-  function useMockCurrentPosition() {
-    const state = React.useState([])
-    setReturnValue = state[1]
-    return state[0]
-  }
-
-  useCurrentPosition.mockImplementation(useMockCurrentPosition)
+  const { promise, resolve } = deferred()
+  window.navigator.geolocation.getCurrentPosition.mockImplementation(
+    callback => {
+      promise.then(() => callback(fakePosition))
+    },
+  )
 
   render(<Location />)
 
   expect(screen.getByLabelText(/loading/i)).toBeInTheDocument()
 
-  act(() => {
-    return setReturnValue([fakePosition])
+  await act(() => {
+    resolve()
+    return promise
   })
+
   expect(screen.queryByLabelText(/loading/i)).not.toBeInTheDocument()
 
   expect(screen.getByText(/latitude/i)).toHaveTextContent(
-    `Latitude: ${fakePosition.coords.latitude}`
+    `Latitude: ${fakePosition.coords.latitude}`,
   )
-
   expect(screen.getByText(/longitude/i)).toHaveTextContent(
-    `Longitude: ${fakePosition.coords.longitude}`
+    `Longitude: ${fakePosition.coords.longitude}`,
   )
 })
 
-/*
-eslint
-  no-unused-vars: "off",
-*/
+test('displays error message when geolocation is not supported', async () => {
+  const fakeError = new Error(
+    'Geolocation is not supported or permission denied',
+  )
+  const { promise, reject } = deferred()
+
+  window.navigator.geolocation.getCurrentPosition.mockImplementation(
+    (successCallback, errorCallback) => {
+      promise.catch(() => errorCallback(fakeError))
+    },
+  )
+
+  render(<Location />)
+
+  expect(screen.getByLabelText(/loading/i)).toBeInTheDocument()
+
+  await act(async () => {
+    reject()
+  })
+
+  expect(screen.queryByLabelText(/loading/i)).not.toBeInTheDocument()
+
+  expect(screen.getByRole('alert')).toHaveTextContent(fakeError.message)
+})
